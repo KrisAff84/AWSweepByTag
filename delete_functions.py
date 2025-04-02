6593de3690777898b172459ff4343a20334e8d76
KrisAff84 <krisaff@gmail.com>
2025-04-02 15:45:20 -0500

    feat: Initial commit

---
+++ delete_functions.py
@@ -0,0 +1,204 @@
+'''
+Contains the delete functions as well as the DELETE_FUNCTIONS dictionary, which maps resource and service
+to the appropriate individual delete function.
+'''
+
+import json
+import botocore.exceptions
+import boto3
+
+#######################################################################
+# Individual Deletion Functions
+#######################################################################
+
+def delete_lambda_function(arn):
+    client = boto3.client('lambda')
+    response = client.delete_function(FunctionName=arn)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"Lambda function {arn} was successfully deleted")
+    else:
+        print(f"Lambda function {arn} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+def delete_sqs_queue(arn):
+    client = boto3.client('sqs')
+    queue_name = arn.split(':')[-1]
+    queue_url = client.get_queue_url(QueueName=queue_name)['QueueUrl']
+    response = client.delete_queue(QueueUrl=queue_url)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"SQS queue {arn} was successfully deleted")
+    else:
+        print(f"SQS queue {arn} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+##################### EC2 Service #######################
+
+def delete_subnet(arn):
+    client = boto3.client('ec2')
+    subnet_id = arn.split('/')[-1]
+    response = client.delete_subnet(SubnetId=subnet_id)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"Subnet {subnet_id} was successfully deleted")
+    else:
+        print(f"Subnet {subnet_id} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+def delete_vpc_endpoint(arn):
+    client = boto3.client('ec2')
+    endpoint_id = arn.split('/')[-1]
+    try:
+        response = client.delete_vpc_endpoints(VpcEndpointIds=[endpoint_id])
+
+        # Check for any errors in the response
+        if 'Unsuccessful' in response:
+            for error in response['Unsuccessful']:
+                if 'Error' in error and error['Error']['Code'] == 'InvalidVpcEndpoint.NotFound':
+                    print(f"VPC endpoint {endpoint_id} was already deleted.")
+                    return None  # Skip further actions for this resource as it's already deleted
+
+        # If deletion is successful
+        if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+            print(f"VPC endpoint {endpoint_id} was successfully deleted")
+        else:
+            print(f"VPC endpoint {endpoint_id} was not successfully deleted")
+
+        # Print the full response for debugging
+        print(json.dumps(response, indent=4, default=str))
+
+    except botocore.exceptions.ClientError as e:
+        print(f"Failed to delete VPC endpoint {endpoint_id}: {str(e)}")
+        return None
+
+def delete_internet_gateway(arn):
+    client = boto3.client('ec2')
+    gateway_id = arn.split('/')[-1]
+
+    # Detach Internet Gateway if it is attached to a VPC
+    try:
+        response = client.describe_internet_gateways(InternetGatewayIds=[gateway_id])
+        attachments = response.get('InternetGateways', [])[0].get('Attachments', [])
+
+        for attachment in attachments:
+            vpc_id = attachment.get('VpcId')
+            if vpc_id:
+                client.detach_internet_gateway(InternetGatewayId=gateway_id, VpcId=vpc_id)
+
+    except botocore.exceptions.ClientError as e:
+        print(f"DEBUG: Failed to detach Internet Gateway {gateway_id}, error: {str(e)}")
+        return
+
+    # Delete Internet Gateway after it has been detached
+    try:
+        response = client.delete_internet_gateway(InternetGatewayId=gateway_id)
+        if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+            print(f"Internet gateway {gateway_id} was successfully deleted")
+        else:
+            print(f"Internet gateway {gateway_id} was not successfully deleted")
+        print(json.dumps(response, indent=4, default=str))
+
+    except botocore.exceptions.ClientError as e:
+        print(f"Failed to delete {gateway_id}: {str(e)}")
+
+def delete_route_table(arn):
+    client = boto3.client('ec2')
+    route_table_id = arn.split('/')[-1]
+    response = client.delete_route_table(RouteTableId=route_table_id)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"Route table {route_table_id} was successfully deleted")
+    else:
+        print(f"Route table {route_table_id} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+def delete_vpc(arn):
+    client = boto3.client('ec2')
+    vpc_id = arn.split('/')[-1]
+    response = client.delete_vpc(VpcId=vpc_id)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"VPC {vpc_id} was successfully deleted")
+    else:
+        print(f"VPC {vpc_id} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+def delete_nat_gateway(arn):
+    client = boto3.client('ec2')
+    nat_gateway_id = arn.split('/')[-1]
+    deleted = client.describe_nat_gateways(NatGatewayIds=[nat_gateway_id])['NatGateways'][0]['State']
+    if deleted == 'deleted' or deleted == 'deleting':
+        print(f"Nat gateway {nat_gateway_id} was already deleted")
+        return
+    response = client.delete_nat_gateway(NatGatewayId=nat_gateway_id)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"Nat gateway {nat_gateway_id} was successfully deleted")
+    else:
+        print(f"Nat gateway {nat_gateway_id} was not successfully deleted")
+    print(json.dumps(response, indent=4, default=str))
+
+def release_eip(arn):
+    client = boto3.client('ec2')
+    allocation_id = arn.split('/')[-1]
+    response = client.release_address(AllocationId=allocation_id)
+    if 200 <= response['ResponseMetadata']['HTTPStatusCode'] < 300:
+        print(f"Elastic IP {allocation_id} was successfully released")
+    else:
+        print(f"Elastic IP {allocation_id} was not successfully released")
+    print(json.dumps(response, indent=4, default=str))
+
+###################################################################
+# Delete function mappings
+###################################################################
+
+DELETE_FUNCTIONS = {
+    'ec2': {
+        'subnet': delete_subnet,
+        'vpcendpoint': delete_vpc_endpoint,
+        'internetgateway': delete_internet_gateway,
+        'transitgatewayattachment': lambda resource: print("deleting transit gateway attachment"),  # delete_transit_gateway_vpc_attachment(resource['arn'])
+        'natgateway': delete_nat_gateway,  # delete_nat_gateway(resource['arn'])
+        'route': lambda resource: print("deleting route"),  # delete_route(resource['arn'])
+        'routetable': delete_route_table,
+        'security_group': lambda resource: print("deleting security group"),  # delete_security_group(resource['arn'])
+        'vpc': delete_vpc,
+        'vpcpeering': lambda resource: print("deleting vpc peering"),  # delete_vpc_peering_connection(resource['arn'])
+        'eip': release_eip
+    },
+    'certificatemanager': {
+        'certificate': lambda resource: print("deleting certificate"),  # delete_certificate(resource['arn'])
+    },
+    'route53': {
+        'hostedzone': lambda resource: print("deleting hosted zone"),  # delete_hosted_zone(resource['arn'])
+    },
+    'sqs': {
+        'queue': delete_sqs_queue
+    },
+    'sns': {
+        'topic': lambda resource: print("deleting topic"),  # delete_topic(resource['arn'])
+    },
+    's3': {
+        'bucket': lambda resource: print("deleting bucket"),  # delete_bucket(resource['XXX'])
+    },
+    'lambda': {
+        'function': delete_lambda_function
+    },
+    'dynamodb': {
+        'table': lambda resource: print("deleting table"),  # delete_table(resource['arn'])
+    },
+    'cloudfront': {
+        'distribution': lambda resource: print("deleting distribution"),  # delete_distribution(resource['arn'])
+    },
+    'apigateway': {
+        'restapi': lambda resource: print("deleting rest api"),  # delete_rest_api(resource['arn'])
+    },
+    'kms': {
+        'key': lambda resource: print("deleting key"),  # delete_key(resource['arn'])
+    },
+    'iam': {
+        'role': lambda resource: print("deleting role"),  # delete_role(resource['arn'])
+        'policy': lambda resource: print("deleting policy"),  # delete_policy(resource['arn'])
+    },
+    'elasticloadbalancing': {
+        'loadbalancer': lambda resource: print("deleting load balancer"),  # delete_load_balancer(resource['arn'])
+    },
+    'secretsmanager': {
+        'secret': lambda resource: print("deleting secret"),  # delete_secret(resource['arn'])
+    }
+}
---
+++ main.py
@@ -0,0 +1,145 @@
+import json
+import time
+import boto3
+import botocore.exceptions
+from delete_functions import DELETE_FUNCTIONS
+
+
+def get_resources_by_tag(tag_key, tag_value):
+    '''' Gets list of resources by common tag key and value. '''
+    client = boto3.client('resource-groups')
+
+    query = {
+        "Type": "TAG_FILTERS_1_0",
+        "Query": json.dumps({
+            "ResourceTypeFilters": ["AWS::AllSupported"],
+            "TagFilters": [
+                {
+                    "Key": tag_key,
+                    "Values": [tag_value]
+                }
+            ]
+        })
+    }
+
+    resources = []
+    response = client.search_resources(ResourceQuery=query)
+    while True:
+        resources.extend(response.get('ResourceIdentifiers', []))
+        next_token = response.get('NextToken')
+
+        if not next_token:
+            break
+
+        response = client.search_resources(ResourceQuery=query, NextToken=next_token)
+
+    return resources
+
+
+def parse_resource_by_type(resource):
+    """Parses resource type, service and ARN from each resource to find the appropriate delete function."""
+    arn = resource['ResourceArn']
+    service = (resource['ResourceType'].split('::')[1]).lower()
+    resource_type = (resource['ResourceType'].split('::')[2]).lower()
+    resource_for_deletion = {
+        'resource_type': resource_type,
+        'arn': arn,
+        'service': service
+    }
+    return resource_for_deletion
+
+
+def delete_resource(resource):
+    """Finds and calls the appropriate delete function based on the resource type."""
+    service = resource['service']
+    resource_type = resource['resource_type']
+    arn = resource['arn']
+
+    # print(f"DEBUG: Checking DELETE_FUNCTIONS for service='{service}', resource_type='{resource_type}'")
+
+    if service in DELETE_FUNCTIONS and resource_type in DELETE_FUNCTIONS[service]:
+        try:
+            # print(f"DEBUG: Calling delete function for {service}::{resource_type}")
+            DELETE_FUNCTIONS[service][resource_type](arn)
+
+        except botocore.exceptions.ClientError as e:
+            if "DependencyViolation" in str(e):
+                return resource
+            else:
+                print(f"Failed to delete {arn}")
+                return None
+
+    else:
+        print(f"No delete function found for {service}::{resource_type}")
+        return None
+
+
+def retry_failed_deletions(failed_resources, max_retries=3, wait_time=10):
+    """Retries failed deletions up to max_retries times with exponential backoff."""
+
+    for attempt in range(1, max_retries + 1):
+        print(f"\nRetry attempt {attempt}/{max_retries} for failed deletions...\n")
+        new_failed_resources = []
+
+        for resource in failed_resources:
+            if isinstance(resource, str):  # Skip if the resource is a string
+                print(f"Skipping invalid resource: {resource}")
+                continue
+
+            try:
+                result = delete_resource(resource)  # Returns failed resources if deletion still fails
+                if result:
+                    new_failed_resources.extend(result)  # Collect still-failed resources
+            except botocore.exceptions.ClientError as e:
+                if "DependencyViolation" in str(e):
+                    new_failed_resources.append(resource)
+                else:
+                    print(f"Failed to delete {resource['arn']}")
+
+        if not new_failed_resources:  # If everything was deleted, exit early
+            print("All failed deletions were successfully retried.")
+            return
+
+        new_failed_resources.reverse()  # Reverse in place
+        failed_resources = new_failed_resources  # Update failed resources
+        print(f"{len(failed_resources)} resources still cannot be deleted. Retrying in {wait_time} seconds...")
+        time.sleep(wait_time)  # Wait before retrying
+
+    print(f"\nFinal retry attempt reached. {len(failed_resources)} resources could not be deleted.\n")
+
+
+def main():
+    tag_key = input("Enter the tag key to search by: ")
+    tag_value = input("Enter the tag value to search by: ")
+    resources = get_resources_by_tag(tag_key, tag_value)
+
+    print("\n Resources queued for deletion: \n")
+    resources_for_deletion = []
+
+    for resource in resources:
+        resource_for_deletion = parse_resource_by_type(resource)
+        print(json.dumps(resource_for_deletion, indent=2))
+        resources_for_deletion.append(resource_for_deletion)
+
+    print(f"\n{len(resources)} resources queued for deletion. \n")
+    delete = input("Are you sure you want to delete all of these resources? (y/n): \n")
+
+    if delete.lower() != 'y':
+        print("Exiting...")
+        return
+
+    print("Deleting resources... \n")
+    failed_deletions = []
+
+    for resource in resources_for_deletion:
+        failed_deletion = delete_resource(resource)
+        if failed_deletion:
+            failed_deletions.append(failed_deletion)
+
+    if failed_deletions:
+        print("\n Retrying failed deletions...")
+        retry_failed_deletions(failed_deletions)
+
+
+if __name__ == '__main__':
+    main()
